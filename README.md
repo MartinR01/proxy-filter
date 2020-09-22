44e60c799bf8a9f9003be63bfd13d4231e3cfa3a
Martin Ryba <martin.ryba01@gmail.com>
2020-09-22 18:18:01 +0200

    Basic prototype for proxy server. Needs to be split up into classes and most cases are not handled.
    Included some basic instructions for testing as well.

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # fake-news
 Proxy server that changes content of HTTP traffic.
+
+# Tests
+## Scenario 1
+- after running the proxy, telnet into it:
+```
+telnet localhost 8080
+```
+- After successful connection, paste the following request to get simple text through HTTP
+**INCLUDING THE EMPTY LINE**:
+```
+GET /fakenews/test1.txt HTTP/1.1
+host: zebroid.ida.liu.se
+
+```
+- The response should look like this:
+```
+HTTP/1.1 200 OK
+Date: Tue, 22 Sep 2020 16:08:35 GMT
+Server: Apache/2.4.6 (CentOS) mod_auth_gssapi/1.5.1 mod_nss/1.0.14 NSS/3.28.4 mod_wsgi/3.4 Python/2.7.5
+Last-Modified: Sun, 30 Aug 2020 16:17:02 GMT
+ETag: "17f-5ae1a9db7bb80"
+Accept-Ranges: bytes
+Content-Length: 383
+Content-Type: text/plain; charset=UTF-8
+
+This is a basic text file.
+It tells a simple story about our yellow friend Smiley,
+who is from Stockholm. Smiley is round, I think.
+
+Without your proxy, you should be able to view this page just fine.
+
+With your proxy, this page should look a bit different,
+with all mentions of Smiley from Stockholm
+being changed into something else.  
+The page should still be formatted properly.
+```
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,63 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main{
     public static void main(String[] args){
+        final int PORT = 8080;
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            while (true) {
+                Socket socket = serverSocket.accept();
 
+                System.out.println("creatig streams....");
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+                while (true) {
+                    System.out.println("reading message....");
+                    String s = bufferedReader.readLine();
+                    String msg = "";
+
+                    while (!s.equals("")){
+                        msg += s + "\n";
+                        s = bufferedReader.readLine();
+                    }
+
+                    System.out.println(msg);
+                    String host = msg.split("\n")[1].split(" ")[1];
+                    System.out.println(host);
+
+                    Socket clientSocket = new Socket(host, 80);
+                    BufferedWriter clientWriter = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+                    BufferedReader clientReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    System.out.println("Created client socket");
+
+                    msg = msg.replace("\n", "\r\n") + "\r\n";
+                    System.out.println("masg: "+ msg);
+
+                    clientWriter.write(msg);
+                    clientWriter.flush();
+                    System.out.println("sent message to the server");
+
+                    String r = clientReader.readLine();
+                    String response = "";
+
+                    while (r != null){
+                        response += r + "\n";
+                        r = clientReader.readLine();
+                    }
+                    System.out.println("Read the response");
+
+                    System.out.println(response);
+                    System.out.println("-----------");
+
+                    bufferedWriter.write(response);
+                    bufferedWriter.flush();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
