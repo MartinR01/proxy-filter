384acceed62a279df713a99447a70f16106bb5da
Martin Ryba <martin.ryba01@gmail.com>
2020-10-08 12:27:17 +0200

    Added message on successful start.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import conn.Server;
 public class Main{
     public static void main(String[] args){
         Server server = Server.getInstance();
+        System.out.println("Running...");
 
         while(true){
             server.acceptConnection();
